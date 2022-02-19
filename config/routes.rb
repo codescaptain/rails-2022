8390ced37c60f1929b2b3563842355da4343635d
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2022-02-19 12:00:04 +0300

    add devise and roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'user' => 'pages#user'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
